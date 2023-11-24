1f899f1f440721f26bb5b56aac17761b03b35351
Mohan raj <147843991+Mohanguvi@users.noreply.github.com>
2023-11-25 01:18:27 +0530

    Read the text file
    The program that reads the given text file and displays the name of the text file and display the contents also

---
+++ Program_2.py
@@ -0,0 +1,5 @@
+
+# A file is created and has its own content 
+with open("Program-2.txt", "r") as file:
+    content = file.read()
+    print("File name is program-2.txt and the content is :", content)
